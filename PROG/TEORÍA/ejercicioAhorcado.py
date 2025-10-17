bbff5f0c5fc1424ba4c3a8983a8b698c10453f5e
Juan <j.martinezgonzalez-besada@danielcastelao.org>
2025-10-17 11:07:30 +0200

    Ahorcado

--- PROG/TEORÍA/ejercicioAhorcado.py
+++ PROG/TEORÍA/ejercicioAhorcado.py
@@ -5,25 +5,39 @@ palabraGuions=''
 for letra in palabra:
     palabraGuions = palabraGuions + '_'
 
-while acerto == False or fallos <= 7:
+while acerto == False and fallos <= 7:
     print("A palabra a acertar é: "+ palabraGuions)
+    print("Ten", str(7-fallos),"oportunidades")
     palabraLetra = input("Introduce la palabra o la letra: ")
-    if len(palabraLetra) != 0:
-        if palabraLetra != 1:
+    if len(palabraLetra) != 1:
+        if len(palabraLetra) != 1:
             if palabraLetra == palabra:
                 acerto=True
                 print("Felicidades, acertaches")
+                break
             else:
+                print(palabraLetra,"non é correcta")
                 fallos= fallos + 1
+                if fallos == 7:
+                    break
+    else:
+        encontrado = False
+        for i, caracter in enumerate(palabra):
+             if palabraLetra == caracter:
+                palabraGuions = palabraGuions[:i] + caracter + palabraGuions[i+1:]
+                encontrado = True
+        if encontrado == True:
+             if palabraGuions == palabra:
+                print("Noraboa, acertaches a palabra" + palabra)
+                acerto=True
         else:
-            encontrado = False
-            for i, caracter in enumerate(palabra):
-                if palabraLetra == caracter:
-                    palabraGuions = palabraGuions[:i] + caracter + palabraGuions
-                    encontrado = True
-            if encontrado == True:
-                if palabraGuions == palabra:
-                    print("Noraboa, acertaches a palabra" + palabra)
-                    acerto=True
-            else:
-                fallos = fallos + 1
+            fallos = fallos + 1
+def eliminar_acentos(palabra):
+    for i, letra in  enumerate(palabra):
+        if "á" in letra:
+            palabra = palabra.replace('á','a')
+            palabra= palabra[:i] + 'a' + palabra[i+1:]
+
+
+
+
